1f14410026da7a8354c4325beb0a1a35810cb5a1
raghavtwenty <126254197+raghavtwenty@users.noreply.github.com>
2024-10-24 11:17:41 +0530

    added save in file

---
+++ _0002_file_output.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+: '
+Filename: _0002_file_output.sh
+Title: Write output into a file
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: October 4, 2024 | Last Updated: October 4, 2024
+Language: Bash 
+'
+echo "Hello world" > file.txt

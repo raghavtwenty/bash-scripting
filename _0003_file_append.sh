ab7cd7d85f6434a774cd8cbd3fd101a9fc925769
raghavtwenty <126254197+raghavtwenty@users.noreply.github.com>
2024-10-24 11:18:08 +0530

    added extend to file

---
+++ _0003_file_append.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+: '
+Filename: _0003_file_append.sh
+Title: Append output in the existing file
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: October 4, 2024 | Last Updated: October 4, 2024
+Language: Bash 
+'
+
+echo "hi how are you" >> file.txt

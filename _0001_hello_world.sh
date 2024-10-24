97bc2a85847abf2cde8689cc874ff03750291c7a
raghavtwenty <126254197+raghavtwenty@users.noreply.github.com>
2024-10-24 11:17:14 +0530

    Added hello world

---
+++ _0001_hello_world.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+: '
+Filename: _0001_hello_world.sh
+Title: Hello world in Bash scripting
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: October 4, 2024 | Last Updated: October 4, 2024
+Language: Bash 
+'
+echo "Hello World"
